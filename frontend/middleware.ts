94c57c0deefc19dc6015dd8b78675b2d16ca3b9e
Muneerali199 <alimuneerali245@gmail.com>
2025-04-20 12:09:23 +0530

    some change

--- frontend/middleware.ts
+++
@@ -1,9 +0,0 @@
-// middleware.ts
-import { clerkMiddleware } from '@clerk/nextjs/server';
-
-export default clerkMiddleware();
-
-// If you're using the `app` directory, match all routes:
-export const config = {
-  matcher: ['/((?!_next|.*\\..*).*)'],
-};
